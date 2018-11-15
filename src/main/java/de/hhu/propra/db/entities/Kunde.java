3820cc6351b90bd3766fc5ceeaae7ff570286282
kingfishbrain <romancherkovsky@googlemail.com>
2018-11-15 15:06:41 +0100

    add OneToMany annotation

--- src/main/java/de/hhu/propra/db/entities/Kunde.java
+++ src/main/java/de/hhu/propra/db/entities/Kunde.java
@@ -2,10 +2,7 @@ package de.hhu.propra.db.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class Kunde {
     private Integer plz;
 
     // TODO: Hier muss irgendetwas mit Autos hin...
-
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
     private List<Auto> autos;
 
 }
